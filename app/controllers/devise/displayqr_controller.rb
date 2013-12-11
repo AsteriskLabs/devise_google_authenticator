3c6c097f131574cb78181e37bd4923fab2b67783
Ron <ronald.arias@qmee.com>
2013-12-11 14:20:02 +0000

    Adding compatibility with Rails 4. Keeping compatibility with rails 3. Adding support for strong parameters. Fixing form_for which does not allow nil objects(resource). Fixing generators.

--- app/controllers/devise/displayqr_controller.rb
+++ app/controllers/devise/displayqr_controller.rb
@@ -1,20 +1,19 @@
 class Devise::DisplayqrController < DeviseController
   prepend_before_filter :authenticate_scope!, :only => [:show,:update]
-  
+
   include Devise::Controllers::Helpers
-  
+
   def show
-    if not resource.nil? and not resource.gauth_secret.nil?
-      render :show
-    else
+    if resource.nil? || resource.gauth_secret.nil?
       sign_in scope, resource, :bypass => true
       redirect_to stored_location_for(scope) || :root
+    else
+      render :show
     end
   end
-  
+
   def update
-    tmp = params[resource_name]
-    if resource.set_gauth_enabled(params[resource_name])
+    if resource.set_gauth_enabled(resource_params)
       set_flash_message :notice, "Status Updated!"
       sign_in scope, resource, :bypass => true
       redirect_to stored_location_for(scope) || :root
@@ -22,14 +21,23 @@ class Devise::DisplayqrController < DeviseController
       render :show
     end
   end
-    
+
   private
   def scope
     resource_name.to_sym
   end
-  
+
   def authenticate_scope!
     send(:"authenticate_#{resource_name}!")
     self.resource = send("current_#{resource_name}")
+  end
+
+  def resource_params
+    return params.require(resource_name.to_sym).permit(:gauth_enabled) if strong_parameters_enabled?
+    params
+  end
+
+  def strong_parameters_enabled?
+    defined?(ActionController::StrongParameters)
   end
 end
