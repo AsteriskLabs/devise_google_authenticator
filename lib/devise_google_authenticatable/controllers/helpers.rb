a614a6aa600913bf2232a739effdee513b80013d
Michael Guymon <michael.guymon@gmail.com>
2014-01-14 13:47:52 -0500

    put qualifier in the right place, add test for setting qualifier in otpauth_user

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -3,15 +3,17 @@ module DeviseGoogleAuthenticator
     module Helpers # :nodoc:
       def google_authenticator_qrcode(user,qualifier=nil)
         username = username_from_email(user.email)
-        if qualifier
-            username << qualifier
-        end
-        data = "otpauth://totp/#{username}@#{Rails.application.class.parent_name}?secret=#{user.gauth_secret}"
+        app = Rails.application.class.parent_name
+        data = "otpauth://totp/#{otpauth_user(username, app, qualifier)}?secret=#{user.gauth_secret}"
         data = Rack::Utils.escape(data)
         url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
         return image_tag(url, :alt => 'Google Authenticator QRCode')
       end
       
+      def otpauth_user(username, app, qualifier=nil)
+        "#{username}@#{app}#{qualifier}"
+      end 
+
       def username_from_email(email)
         (/^(.*)@/).match(email)[1]
       end
