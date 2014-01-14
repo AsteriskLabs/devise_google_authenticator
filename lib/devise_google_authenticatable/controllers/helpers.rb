7c69b49facd892888706e8e20764dd5fdc6f6e84
Michael Guymon <michael.guymon@gmail.com>
2014-01-14 13:10:39 -0500

    allow optional for a qualifier in the username of the otpauth

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -1,8 +1,12 @@
 module DeviseGoogleAuthenticator
   module Controllers # :nodoc:
     module Helpers # :nodoc:
-      def google_authenticator_qrcode(user)
-        data = "otpauth://totp/#{username_from_email(user.email)}@#{Rails.application.class.parent_name}?secret=#{user.gauth_secret}"
+      def google_authenticator_qrcode(user,qualifier=nil)
+        username = username_from_email(user.email)
+        if qualifier
+            username << qualifier
+        end
+        data = "otpauth://totp/#{username}@#{Rails.application.class.parent_name}?secret=#{user.gauth_secret}"
         data = Rack::Utils.escape(data)
         url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
         return image_tag(url, :alt => 'Google Authenticator QRCode')
