fa117e4eb3304d66687a9cc6810aa194bdceba44
zhenyuchen <zhenyu.chen@yottaa.com>
2014-03-12 16:06:12 +0800

    fix the forgotten .rb suffix and replace the tab with space

--- lib/generators/devise_google_authenticator/devise_google_authenticator_generator.rb
+++ lib/generators/devise_google_authenticator/devise_google_authenticator_generator.rb
@@ -1,26 +1,26 @@
 module DeviseGoogleAuthenticator
-	module Generators
-		class DeviseGoogleAuthenticatorGenerator < Rails::Generators::NamedBase
+  module Generators
+    class DeviseGoogleAuthenticatorGenerator < Rails::Generators::NamedBase
 
-			namespace "devise_google_authenticator"
+      namespace "devise_google_authenticator"
 
-			desc "Add :google_authenticatable directive in the given model, plus accessors. Also generate migration for ActiveRecord"
+      desc "Add :google_authenticatable directive in the given model, plus accessors. Also generate migration for ActiveRecord"
 
-			def inject_devise_google_authenticator_content
-				path = File.join("app","models","#{file_path}.rb")
+      def inject_devise_google_authenticator_content
+        path = File.join("app","models","#{file_path}.rb")
 
-				if File.exists?(path)
-				  inject_into_file(path, "google_authenticatable, :", :after => "devise :")
-				  inject_into_file(path, "gauth_enabled, :gauth_tmp, :gauth_tmp_datetime, :", :after => "attr_accessible :") if needs_attr_accessible?
-				  inject_into_class(path, class_name, "\tattr_accessor :gauth_token\n")
-				end
-			end
+        if File.exists?(path)
+          inject_into_file(path, "google_authenticatable, :", :after => "devise :")
+          inject_into_file(path, "gauth_enabled, :gauth_tmp, :gauth_tmp_datetime, :", :after => "attr_accessible :") if needs_attr_accessible?
+          inject_into_class(path, class_name, "\tattr_accessor :gauth_token\n")
+        end
+      end
 
-			hook_for :orm
+      hook_for :orm
 
-			private
+      private
 
-			def needs_attr_accessible?
+      def needs_attr_accessible?
         rails_3? && !strong_parameters_enabled?
       end
 
@@ -32,6 +32,6 @@ module DeviseGoogleAuthenticator
         defined?(ActionController::StrongParameters)
       end
 
-		end
-	end
+    end
+  end
 end
