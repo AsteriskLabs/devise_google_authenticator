e61ff9a7047a943ecd7020a3d9da46a67814ceb7
Matt Nodurfth <mnodurft@gmail.com>
2014-05-21 11:21:06 +0800

    updating the controller

--- app/controllers/devise/checkga_controller.rb
+++ app/controllers/devise/checkga_controller.rb
@@ -16,29 +16,34 @@ class Devise::CheckgaController < Devise::SessionsController
   def update
     resource = resource_class.find_by_gauth_tmp(params[resource_name]['tmpid'])
 
-    if not resource.nil?
+    if resource.class.name.downcase.to_sym == :player
+      path = player_checkga_path(id: params[:player][:tmpid])
+    else
+      path = affiliate_checkga_path(id: params[:affiliate][:tmpid])
+    end
 
+    if not resource.nil?
       if resource.validate_token(params[resource_name]['gauth_token'].to_i)
         set_flash_message(:notice, :signed_in) if is_navigational_format?
         sign_in(resource_name,resource)
         warden.manager._run_callbacks(:after_set_user, resource, warden, {:event => :authentication})
         respond_with resource, :location => after_sign_in_path_for(resource)
 
-        if not resource.class.ga_remembertime.nil? 
+        if not resource.class.ga_remembertime.nil?
           cookies.signed[:gauth] = {
             :value => resource.email << "," << Time.now.to_i.to_s,
             :secure => !(Rails.env.test? || Rails.env.development?),
-            :expires => (resource.class.ga_remembertime + 1.days).from_now
+            :expires => Time.now
           }
         end
       else
         set_flash_message(:error, :error)
-        redirect_to :root
+        redirect_to path
       end
 
     else
       set_flash_message(:error, :error)
-      redirect_to :root
+      redirect_to path
     end
   end
 
