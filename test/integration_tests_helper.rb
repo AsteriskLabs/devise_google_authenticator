98eb9957971f7822f444d1e4fc05963bc9a28050
Christian Frichot <xntrik@gmail.com>
2015-02-08 08:46:37 +0800

    Updated tests - seem to work in both ruby 1.9.3 and 2.1.5

--- test/integration_tests_helper.rb
+++ test/integration_tests_helper.rb
@@ -22,6 +22,7 @@ class ActionController::IntegrationTest
     sign_in_as_user(testuser)
     visit user_displayqr_path
     check 'user_gauth_enabled'
+    fill_in('user_gauth_token', :with => ROTP::TOTP.new(testuser.get_qr).at(Time.now))
     click_button 'Continue...'
 
     Capybara.reset_sessions!
@@ -36,6 +37,6 @@ class ActionController::IntegrationTest
     visit send("new_#{resource_name}_session_path")
     fill_in "#{resource_name}_email", :with => user.email
     fill_in "#{resource_name}_password", :with => user.password
-    click_button 'Sign in'
+    click_button 'Log in'
   end
 end
