f93b2effa9ef89ea23fd5b141fc57e1fd683ad14
Hester van Wijk <h.j.van.wijk@roqua.nl>
2015-11-19 16:28:00 +0100

    removed instance variable in favor of method

--- app/controllers/devise/checkga_controller.rb
+++ app/controllers/devise/checkga_controller.rb
@@ -24,7 +24,7 @@ class Devise::CheckgaController < Devise::SessionsController
         warden.manager._run_callbacks(:after_set_user, resource, warden, {:event => :authentication})
         respond_with resource, :location => after_sign_in_path_for(resource)
 
-        if not resource.class.ga_remembertime.nil? 
+        if not resource.class.ga_remembertime.nil?
           cookies.signed[:gauth] = {
             :value => resource.email << "," << Time.now.to_i.to_s,
             :secure => !(Rails.env.test? || Rails.env.development?),
@@ -43,7 +43,7 @@ class Devise::CheckgaController < Devise::SessionsController
   end
 
   def redirect_on_error_url
-    @redirect_on_error_url ||= :root
+    :root
   end
 
   private
