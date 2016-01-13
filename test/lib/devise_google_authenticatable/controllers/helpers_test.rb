4b1a03c2050d260c4b8728ad22054abd43ca066a
Ed Bosher <ed@bitgamelabs.com>
2016-01-13 19:39:30 +0800

    Use the full email and set an issuer

--- test/lib/devise_google_authenticatable/controllers/helpers_test.rb
+++ test/lib/devise_google_authenticatable/controllers/helpers_test.rb
@@ -8,18 +8,6 @@ class HelpersTest < ActiveSupport::TestCase
 		@user = User.new(valid_attributes({:email => 'helpers_test@test.com' }))
 	end
 
-    test "can get username from user's email" do
-        assert_equal 'helpers_test', username_from_email(@user.email)        
-    end
-
-    test 'can get otpauth_user' do
-        assert_equal "username@app", otpauth_user('username', 'app')
-    end
-
-    test 'can get otpauth_user with a qualifier' do
-        assert_equal "username@app-qualifier", otpauth_user('username', 'app', '-qualifier')
-    end
-
     # fake image tag
     def image_tag(src, *args)
         src
