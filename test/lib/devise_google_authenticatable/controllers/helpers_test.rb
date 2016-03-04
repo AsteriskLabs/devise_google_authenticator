989b6df2bf4d37779b7a117939547637baf5b72a
Jacek Adamek <jacek.adamek@gmail.com>
2016-03-04 13:21:38 +0100

    Updating tests

--- test/lib/devise_google_authenticatable/controllers/helpers_test.rb
+++ test/lib/devise_google_authenticatable/controllers/helpers_test.rb
@@ -6,7 +6,7 @@ class HelpersTest < ActiveSupport::TestCase
 
     def setup
 	    @user = User.new(valid_attributes({:email => 'helpers_test@test.com' }))
-        User.ga_appname = 'SimpleApp'
+        User.ga_appname = 'Simple App'
 	end
 
     # fake image tag
@@ -14,9 +14,9 @@ class HelpersTest < ActiveSupport::TestCase
         src
     end
     test 'generate qrcode' do
-        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimpleApp-test", google_authenticator_qrcode(@user)
-        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimpleApp-test", google_authenticator_qrcode(@user, 'MyQualifier')
-        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimpleApp-test", google_authenticator_qrcode(@user, nil, 'MyIssuer')
-        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimpleApp-test", google_authenticator_qrcode(@user, 'MyQualifier', 'MyIssuer')
+        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimple%2BApp%2B%2528test%2529", google_authenticator_qrcode(@user)
+        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimple%2BApp%2B%2528test%2529", google_authenticator_qrcode(@user, 'MyQualifier')
+        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimple%2BApp%2B%2528test%2529", google_authenticator_qrcode(@user, nil, 'MyIssuer')
+        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimple%2BApp%2B%2528test%2529", google_authenticator_qrcode(@user, 'MyQualifier', 'MyIssuer')
     end
 end
