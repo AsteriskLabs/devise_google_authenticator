93583518d3e4f669143be094dbbc6914d0f2f19a
Jacek Adamek <jacek.adamek@gmail.com>
2016-03-04 13:03:11 +0100

    Escaping issuer name

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -4,6 +4,7 @@ module DeviseGoogleAuthenticator
       def google_authenticator_qrcode(user, qualifier=nil, issuer=nil)
         issuer = user.class.ga_appname
         issuer = "#{issuer}-#{Rails.env}" unless Rails.env.production?
+        issuer = Rack::Utils.escape(issuer)
         data = "otpauth://totp/#{user.email}?secret=#{user.gauth_secret}&issuer=#{issuer}"
         data = Rack::Utils.escape(data)
         url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
