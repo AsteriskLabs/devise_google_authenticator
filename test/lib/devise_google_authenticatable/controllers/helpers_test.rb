6c3bdf5c3dd07287b6fb07a4ee33296792942a02
mauisu <maui.su@bitgamelabs.com>
2017-07-21 15:15:11 +0200

    falling back to master [#136768599]

--- test/lib/devise_google_authenticatable/controllers/helpers_test.rb
+++ test/lib/devise_google_authenticatable/controllers/helpers_test.rb
@@ -26,9 +26,9 @@ class HelpersTest < ActiveSupport::TestCase
         src
     end
     test 'generate qrcode' do
-        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimple%2BApp%2B%2528test%2529", google_authenticator_qrcode(@user)
-        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimple%2BApp%2B%2528test%2529", google_authenticator_qrcode(@user, 'MyQualifier')
-        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimple%2BApp%2B%2528test%2529", google_authenticator_qrcode(@user, nil, 'MyIssuer')
-        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40test.com%3Fsecret%3D%26issuer%3DSimple%2BApp%2B%2528test%2529", google_authenticator_qrcode(@user, 'MyQualifier', 'MyIssuer')
+        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40RailsApp%3Fsecret%3D", google_authenticator_qrcode(@user)
+        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40RailsAppMyQualifier%3Fsecret%3D", google_authenticator_qrcode(@user, 'MyQualifier')
+        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40RailsApp%3Fsecret%3D%26issuer%3DMyIssuer", google_authenticator_qrcode(@user, nil, 'MyIssuer')
+        assert_equal "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2Fhelpers_test%40RailsAppMyQualifier%3Fsecret%3D%26issuer%3DMyIssuer", google_authenticator_qrcode(@user, 'MyQualifier', 'MyIssuer')
     end
 end
