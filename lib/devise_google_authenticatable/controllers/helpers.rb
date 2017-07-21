975bb6281177521f1b808d90f8b32f0694a6e661
mauisu <maui.su@bitgamelabs.com>
2017-07-21 15:06:34 +0200

    falling back to master [#136768599]

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -2,13 +2,21 @@ module DeviseGoogleAuthenticator
   module Controllers # :nodoc:
     module Helpers # :nodoc:
       def google_authenticator_qrcode(user, qualifier=nil, issuer=nil)
-        issuer = user.class.ga_appname
-        issuer = "#{issuer} (#{Rails.env})" unless Rails.env.production?
-        issuer = Rack::Utils.escape(issuer)
-        data = "otpauth://totp/#{user.first_authentication_key}?secret=#{user.gauth_secret}&issuer=#{issuer}"
+        username = username_from_email(user.email)
+        app = user.class.ga_appname || Rails.application.class.parent_name
+        data = "otpauth://totp/#{otpauth_user(username, app, qualifier)}?secret=#{user.gauth_secret}"
+        data << "&issuer=#{issuer}" if !issuer.nil?
         data = Rack::Utils.escape(data)
         url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
         return image_tag(url, :alt => 'Google Authenticator QRCode')
+      end
+
+      def otpauth_user(username, app, qualifier=nil)
+       "#{username}@#{app}#{qualifier}"
+      end
+
+      def username_from_email(email)
+       (/^(.*)@/).match(email)[1]
       end
     end
   end
