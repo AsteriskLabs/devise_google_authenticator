c86f2de04e4027440b6e8025dc17eefda642cc0f
werleo <werleo@users.noreply.github.com>
2018-09-12 11:52:49 +0200

    code review fix for after sign in path

--- app/controllers/devise/checkga_controller.rb
+++ app/controllers/devise/checkga_controller.rb
@@ -21,7 +21,7 @@ class Devise::CheckgaController < Devise::SessionsController
         set_flash_message(:notice, :signed_in) if is_navigational_format?
         sign_in(resource_name,resource)
         warden.manager._run_callbacks(:after_set_user, resource, warden, {:event => :authentication})
-        respond_with resource, :location => stored_location_for(resource) || after_sign_in_path_for(resource)
+        respond_with resource, :location => after_sign_in_path_for(resource)
 
         if not resource.class.ga_remembertime.nil? 
           cookies.signed[:gauth] = {
