37e7f8c010e2c43e0310e5955039bfe3b0a509d5
ritaly <malgorzata.lyczywek@gmail.com>
2019-02-18 15:18:48 +0100

    add ga_skip_validation_if

--- lib/devise_google_authenticatable/models/google_authenticatable.rb
+++ lib/devise_google_authenticatable/models/google_authenticatable.rb
@@ -108,7 +108,7 @@ module Devise # :nodoc:
         def find_by_gauth_tmp(gauth_tmp)
           where(gauth_tmp: gauth_tmp).first
         end
-        ::Devise::Models.config(self, :ga_timeout, :ga_timedrift, :ga_remembertime, :ga_remember_optional, :ga_appname, :ga_bypass_signup)
+        ::Devise::Models.config(self, :ga_timeout, :ga_timedrift, :ga_remembertime, :ga_remember_optional, :ga_appname, :ga_bypass_signup, :ga_skip_validation_if)
       end
     end
   end
