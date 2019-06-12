def1e65a832e5bec54461691c63334fe2c9f3ab1
Matt Tanous <mtanous22@gmail.com>
2019-06-12 11:51:06 -0600

    Update for future compat with Rails 5

--- app/controllers/devise/checkga_controller.rb
+++ app/controllers/devise/checkga_controller.rb
@@ -1,6 +1,6 @@
 class Devise::CheckgaController < Devise::SessionsController
-  prepend_before_filter :devise_resource, :only => [:show]
-  prepend_before_filter :require_no_authentication, :only => [ :show, :update ]
+  prepend_before_action :devise_resource, only: [:show]
+  prepend_before_action :require_no_authentication, only: [ :show, :update ]
 
   include Devise::Controllers::Helpers
 
@@ -21,14 +21,14 @@ class Devise::CheckgaController < Devise::SessionsController
       if resource.validate_token(params[resource_name]['gauth_token'].to_i)
         set_flash_message(:notice, :signed_in) if is_navigational_format?
         sign_in(resource_name,resource)
-        warden.manager._run_callbacks(:after_set_user, resource, warden, {:event => :authentication})
-        respond_with resource, :location => after_sign_in_path_for(resource)
+        warden.manager._run_callbacks(:after_set_user, resource, warden, {event: :authentication})
+        respond_with resource, location: after_sign_in_path_for(resource)
 
         if not resource.class.ga_remembertime.nil? 
           cookies.signed[:gauth] = {
-            :value => resource.email << "," << Time.now.to_i.to_s,
-            :secure => !(Rails.env.test? || Rails.env.development?),
-            :expires => (resource.class.ga_remembertime + 1.days).from_now
+            value: resource.email << "," << Time.now.to_i.to_s,
+            secure: !(Rails.env.test? || Rails.env.development?),
+            expires: (resource.class.ga_remembertime + 1.days).from_now
           }
         end
       else
