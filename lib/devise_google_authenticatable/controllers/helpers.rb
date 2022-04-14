0f210b263402481c69b7a44b0be4eb204743d34f
Edward Caulfield <edward.caulfield@citrin.ch>
2022-04-14 08:39:00 +0200

    Correct deprecated method call

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -3,7 +3,7 @@ module DeviseGoogleAuthenticator
     module Helpers # :nodoc:
       def google_authenticator_qrcode(user, qualifier=nil, issuer=nil)
         username = username_from_email(user.email)
-        app = user.class.ga_appname || Rails.application.class.parent_name
+        app = user.class.ga_appname || Rails.application.class.module_parent_name
         data = "otpauth://totp/#{otpauth_user(username, app, qualifier)}?secret=#{user.gauth_secret}"
         data << "&issuer=#{issuer}" if !issuer.nil?
         data = Rack::Utils.escape(data)
