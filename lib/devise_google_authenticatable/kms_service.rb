f82e9f619d3c515369147d377ad349891d6bd4ca
Jacek Adamek <jacek.adamek@gmail.com>
2022-04-14 09:31:15 +0200

    Adding the kms service credentials

--- lib/devise_google_authenticatable/kms_service.rb
+++ lib/devise_google_authenticatable/kms_service.rb
@@ -4,24 +4,25 @@ require 'google/cloud/kms'
 
 module DeviseGoogleAuthenticator
   class KmsService
-    def initialize(kms_key_name)
+    def initialize(credentials, kms_key_name)
+      @credentials = credentials
       @kms_key_name = kms_key_name
     end
 
     def decrypt(ciphertext)
-      client = Google::Cloud::Kms.new
+      client = Google::Cloud::Kms.new(credentials: credentials)
       response = client.decrypt(kms_key_name, ciphertext)
       response.plaintext
     end
 
     def encrypt(plaintext)
-      client = Google::Cloud::Kms.new
+      client = Google::Cloud::Kms.new(credentials: credentials)
       response = client.encrypt(kms_key_name, plaintext)
       response.ciphertext
     end
 
     private
 
-    attr_reader :kms_key_name
+    attr_reader :credentials, :kms_key_name
   end
 end
